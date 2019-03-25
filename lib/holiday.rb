4e48331be08cfd3817b1e286ae5d03d4c2232d2e
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-25 04:17:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
   puts "#{season.capitalize}:"
   holidays.each do |holiday, supplies|
-    puts "#{holiday.to_split('_').map {|w| w.capitalize}.join('_) }: #{supplies.join(",")
+    puts "#{holiday.to_split('_').map {|w| w.capitalize}.join('_) }: #{supplies.join(",")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
